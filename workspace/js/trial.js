e42def05b49a483a7814d2b5724e1d991e97e5e0
JSSmall <circadai@yahoo.com>
2016-07-28 19:38:53 -0400

    Add files via upload

---
+++ workspace/js/trial.js
@@ -0,0 +1,22 @@
+/*$(document).ready(function(){
+  $("#myModal").show();
+});*/
+
+/*$("#gallery li img").hover(function(){
+    $("#gallery li img").removeClass("change");
+    $(this).addClass("change");
+    $("#main-img").attr("src",$(this).attr("src"));
+});
+
+alert("hello");*/
+
+
+
+
+
+
+function change() {
+var changeText = document.getElementById("statement");
+changeText.innerHTML= "Thanks! We'll get in touch with you soon";
+}
+
